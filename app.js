850b909c6870f5fdf16bb23c890da619fcc2e2cb
Anthony Cavuoti <anthonycavuoti@gmail.com>
2023-07-31 18:00:59 -0700

    Continue to init

---
+++ app.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+const express = require("express");
+let cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3005;
+
+let whitelist = [
+  "",
+  "http://localhost:3000",
+  "https://homeworkdooerpro.com",
+  "https://www.homeworkdooerpro.com",
+];
+let corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("The HDP Server.");
+});
+
+app.listen(port, () => {
+  console.log(`HDP Node Server listening on port: ${port}`);
+});
+//prock update
+
+module.exports = app;
